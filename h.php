4a248f9a7085aa70cf8d2a803191c8afb60c440c
petrgdanskii <78430757+petrgdanskii@users.noreply.github.com>
2022-05-03 00:10:41 +0300

    Create h.php

---
+++ h.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Untitled</title>
+
+</head>
+
+    <body class="page_bg">
+    Привет, сегодня 
+    <?php echo date('l, F jS, Y'); ?>.
+    <?php
+    $say = 'Hello world!';?>
+
+    <?php echo 'Самая распространенная конструкция'; ?>
+</body>
+</html>
